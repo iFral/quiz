b42d4a26cd11f01467d26dc3d81989bfecc83160
Kyle Wilkins <wilkins_kyle@bah.com>
2022-11-03 14:45:38 -0500

    Part 1 completed

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"flag"
+	"fmt"
 	"os"
 
 	log "github.com/sirupsen/logrus"
@@ -14,8 +15,7 @@ func main() {
 	flag.StringVar(&qFile, "f", "problems.csv", "The quiz file to be used.")
 
 	//Read csv
-	log.Infoln("@@@  Reading file  @@@\n" +
-		qFile)
+	log.Infoln("@@@  Reading file", qFile, "  @@@")
 	file, err := os.Open(qFile)
 	if err != nil {
 		log.Errorln("!!!  Error opening quiz file  !!!")
@@ -24,5 +24,24 @@ func main() {
 	reader := csv.NewReader(file)
 	questions, _ := reader.ReadAll()
 
-	log.Info(questions)
+	//Ask questions and get answers
+	log.Infoln("@@@  Asking", len(questions), "question(s)  @@@")
+	var index int = 1
+	var results = make(map[int]bool)
+	for _, q := range questions {
+		fmt.Print(index, ". ", q[0], " = ")
+		var ans string
+		fmt.Scanln(&ans)
+		results[index] = q[1] == ans
+		index++
+	}
+	log.Infoln("@@@  Quiz completed. Tabulating score.  @@@")
+	var correct int = 0
+	for _, r := range results {
+		if r {
+			correct++
+		}
+	}
+	fmt.Println("You got", correct, "of", len(questions), "correct.")
+
 }
